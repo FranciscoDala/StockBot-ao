fa2753b986345776c9d725d69b1f3a0f78c905ee
Francisco Dala <killerbless12@gmail.com>
2026-07-09 21:57:32 +0100

    fix: update separados!

---
+++ apps/bot/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('StockBot AO is running'));
+app.listen(PORT, () => console.log(`HTTP server running on ${PORT}`));
+
+const client = new Client({
+    authStrategy: new LocalAuth({ clientId: "stockbot" }),
+    puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+});
+
+client.on('qr', (qr) => {
+    console.log('================ ESCANEIA ESSE QR ==================');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => console.log('✅ WhatsApp Conectado!'));
+client.initialize();
