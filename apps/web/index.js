cc44366743003b3dadd77debf07f4a17b74d4299
Francisco Dala <killerbless12@gmail.com>
2026-07-09 21:22:57 +0100

    Ajuste para bot no Railway

--- apps/web/index.js
+++ apps/web/index.js
@@ -3,7 +3,15 @@ const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: "stockbot" }),
-    puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
+    }
 });
 
 client.on('qr', (qr) => {
