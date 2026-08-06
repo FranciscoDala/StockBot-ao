c366d080e238d60889a028b46a9a9e5dafe335ea
Francisco Dala <killerbless12@gmail.com>
2026-08-06 12:34:30 +0100

    update: atualizar modo lojas e ajustando as abas --- 7

--- apps/api/app/db/base.py
+++ apps/api/app/db/base.py
@@ -13,7 +13,7 @@ class BaseModel(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now()) # <- TIREI nullable=False
 
 # FORÇA REGISTRAR TODOS MODELS NA ORDEM CERTA
 from app.models.loja import Loja
