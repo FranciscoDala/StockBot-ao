082e26b8eddceb491be1f615375e19421cb47711
Francisco Dala <killerbless12@gmail.com>
2026-08-21 13:51:15 +0100

    update: 21/08/26 ------- ajuste do equipeTab, 13:51m

--- apps/web/app/loja/[id]/page.tsx
+++ apps/web/app/loja/[id]/page.tsx
@@ -90,9 +90,9 @@ export default function LojaPage() {
     const allTabs = [
         { id: "dados", label: "Dados", icon: FileText, show: modo === "completo" || modo === "venda" }, // <- AJUSTADO
         { id: "venda", label: "Venda", icon: ShoppingCart, show: podeVerVendas && (modo === "completo" || modo === "venda") },
-        { id: "produtos", label: "Produtos", icon: Package, show: (podeVerVendas || podeVerEstoque) && (modo === "completo" || modo === "venda") },
+        { id: "produtos", label: "Produtos", icon: Package, show: (podeVerVendas || podeVerEstoque) && (modo === "completo" || modo === "venda" || modo === "cliente" ) },
         { id: "clientes", label: "Clientes", icon: UserRound, show: modo === "completo" || modo === "cliente" },
-        { id: "equipa", label: "Equipa", icon: Users, show: modo === "completo" || modo === "cliente" }, // <- AJUSTADO
+        { id: "equipa", label: "Equipa", icon: Users, show: modo === "completo" || modo === "cliente" || modo === "venda"  }, // <- AJUSTADO
         { id: "fornecedores", label: "Fornecedores", icon: Truck, show: podeVerTudo && (modo === "completo" || modo === "venda") },
         { id: "documentos", label: "Relatórios", icon: FileText, show: podeVerTudo && (modo === "completo" || modo === "venda") }, // <- AJUSTADO
         { id: "estatisticas", label: "Estatisticas", icon: BarChart3, show: modo === "completo" || modo === "venda" }, // <- AJUSTADO
